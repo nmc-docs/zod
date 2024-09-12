7ceada91c9c17ea91d8b581a261f8f2be1897e3e
Nguyễn Minh Chí <minhchico300kc@gmail.com>
2024-09-12 13:16:44 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
         },
         items: [
           {
-            href: "https://github.com/minhchi1509",
+            href: "https://github.com/nmc-docs/zod",
             label: "GitHub",
             position: "right",
           },
